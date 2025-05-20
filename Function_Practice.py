5a8b6880f032d6a34ff5e0423fa8c59a779ea528
Nicholas Hamilton <hamiltonn@berea.edu>
2025-05-19 20:14:57 -0400

    Quick commit

---
+++ Function_Practice.py
@@ -0,0 +1,84 @@
+
+################################################################################################
+# Assignment: About Parameters of Functions
+# Name: Nicholas Hamilton
+# This project is the ninth assignment in my GEN AI Externship with Cognizant.
+# No AI is used in this project.
+################################################################################################
+
+
+def greet_user(name):
+
+    print(f'Hello {name}, welcome to the function practice!')
+
+
+def describe_pet(pet_name='default_dog', animal_type='dog'):
+    print(f'My pet is a {animal_type} and its name is {pet_name}.')
+
+
+def make_sandwich(*args):
+
+    """
+    This function takes a list of ingredients and prints them out
+    """
+    print('Making a sandwich with the following ingredients:')
+    for ingredient in args:
+        count = args.index(ingredient) + 1
+        print(f'{count}. {ingredient}')
+
+
+def factorial_function(n):
+    if n == 1:
+        return 1
+    return n * factorial_function(n - 1)
+
+
+
+
+def add_numbers(num1, num2):
+    """
+    This function takes two numbers and returns their sum, simple
+    """
+    return num1 + num2
+
+def main():
+    """
+    simple function practice
+
+    """
+
+    users_name = input('What is your name? ')
+    greet_user(users_name)
+
+    num1 = 5
+    num2 = 10
+    print(f' the sum of num1 and num 2 is {add_numbers(num1, num2)}')
+
+    # task 2
+    pet_name = 'rex'
+    animal_type = 'dog'
+
+    describe_pet(pet_name, animal_type)
+
+    make_sandwich('lettuce', 'tomato', 'turkey', 'mayo')
+
+    test_number = 5
+    print(f'the factoral of {test_number} is {factorial_function(test_number)}')
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
